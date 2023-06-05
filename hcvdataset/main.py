781c3b5c91bba31263a4d736823d2933956c7021
rajwrdhn <rajwrdhn@gmail.com>
2023-06-05 13:56:48 +0200

    dockerize and changes

--- hcvdataset/main.py
+++ hcvdataset/main.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.ensemble import IsolationForest
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
 from math import sqrt
 
@@ -24,19 +24,22 @@ def preprocess():
 
     # split into train and test sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-    print(X_train.shape, y_train.shape)
+    #print(X_train.shape, y_train.shape)
     #identify outliers in the training set
     iso = IsolationForest(contamination=0.1)
     yhat = iso.fit_predict(X_train)
     mask = yhat != -1
     X_train, y_train = X_train[mask, :], y_train[mask]
-    print(X_train.shape, y_train.shape)
+    #print(X_train.shape, y_train.shape)
     # fit the model
-    model = LinearRegression()
+    model = RandomForestRegressor(n_estimators=1000)
     model.fit(X_train, y_train)
     # evaluate the model
     evaluate(model, X_test, y_test)
     yhat = model.predict(X_test)
+    # evaluate predictions
+    #mae = mean_absolute_error(y_test, yhat)
+    #print('MAE: %.3f' % mae)
 
 def evaluate(model, X_test, y_test):
     predicted = model.predict(X_test)
