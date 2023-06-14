249cd8d88e087cb2ad68098d8c5c2c8c7f190cde
rajwrdhn <rajwrdhn@gmail.com>
2023-06-14 05:52:35 +0200

    feature engineering

--- hcvdataset/main.py
+++ hcvdataset/main.py
@@ -9,10 +9,28 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
 from math import sqrt
+#import seaborn as sn
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
 
 def preprocess():
     df = pd.read_csv("hcv.csv")
     df = df.drop(columns=["Unnamed: 0"])
+
+    #variance
+    #print(df.var(axis=0))
+
+    #average
+    #print(df.mean(axis=0))
+
+    #correlation matrix RNA
+    #corr_matrix = df.corr()
+    #sn.heatmap(corr_matrix, annot=True)
+    #plt.show()
+
+    #chi2
+
+
 
     #retrieve the array
     data = df.values
@@ -20,10 +38,15 @@ def preprocess():
     # split into input and output elements
     X, y = data[:,:-1], data[:,-1]  
 
+    chi2_selector = SelectKBest(chi2, k=10)
+    X_kbest = chi2_selector.fit_transform(X, y)
+    print(X_kbest)
+    print('Original number of features:', X.shape)
+    print('Reduced number of features:', X_kbest.shape)
     #X, y = df.iloc[:,:-1], df.iloc[:,-1]
 
     # split into train and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    X_train, X_test, y_train, y_test = train_test_split(X_kbest, y, test_size=0.3)
     #print(X_train.shape, y_train.shape)
     #identify outliers in the training set
     iso = IsolationForest(contamination=0.1)
